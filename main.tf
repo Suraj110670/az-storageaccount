6fe7d4c653721b49789fcc691f2bb73d7803e337
Suraj110670 <73783488+Suraj110670@users.noreply.github.com>
2024-01-10 13:44:40 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageaccount"
+  name                     = "sj110670torageaccount"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
